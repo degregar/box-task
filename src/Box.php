42ee30aace07aa6aa8630f6c832e22f21d884ca6
Michał Kukla <degregar@gmail.com>
2019-12-03 12:02:04 +0100

    Tests and implementation of Box class

--- src/Box.php
+++ src/Box.php
@@ -4,11 +4,53 @@ namespace Eselt;
 
 class Box
 {
-    public function __construct($x, $y, $z) {
-        // @TODO
+    private $x;
+    private $y;
+    private $z;
+
+    /**
+     * @param float $x
+     * @param float $y
+     * @param float $z
+     */
+    public function __construct(float $x, float $y, float $z)
+    {
+        if ($x <= 0 || $y <= 0 || $z <= 0) {
+            throw new \InvalidArgumentException("Dimensions of the box must be greater than 0!");
+        }
+
+        $this->x = $x;
+        $this->y = $y;
+        $this->z = $z;
+    }
+
+    public function getX(): float
+    {
+        return $this->x;
+    }
+
+    public function getY(): float
+    {
+        return $this->y;
+    }
+
+    public function getZ(): float
+    {
+        return $this->z;
     }
     
-    public function fitsInto($otherBox) {
-        // @TODO
+    public function fitsInto(Box $otherBox): bool
+    {
+        if ($this->x >= $otherBox->getX()) {
+            return false;
+        }
+        if ($this->y >= $otherBox->getY()) {
+            return false;
+        }
+        if ($this->z >= $otherBox->getZ()) {
+            return false;
+        }
+
+        return true;
     }
 }
--- tests/BoxTest.php
+++ tests/BoxTest.php
@@ -4,11 +4,74 @@ use Eselt\Box;
 
 class BoxTest extends \PHPUnit\Framework\TestCase
 {
-    public function test_it_can_be_instantiated()
+    public function test_it_can_be_instantiated(): Box
     {
         $box = new Box(10, 5, 20);
         $this->assertNotNull($box);
+
+        return $box;
+    }
+
+    public function invalidSizesProvider()
+    {
+        return [
+            [0, 0, 0],
+
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1],
+
+            [1, 1, -1],
+            [1, -1, 1],
+            [-1, 1, 1],
+        ];
+    }
+
+    /**
+     * Box dimensions cannot be 0 or less
+     * 
+     * @dataProvider invalidSizesProvider
+     */
+    public function test_invalid_box_sizes($x, $y, $z): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Box($x, $y, $z);
     }
     
-    // @TODO
+    public function sizesDoesFitProvider(): array
+    {
+        return [
+            [9.9, 4.9, 19.9],
+            [9, 4, 19],
+            [1, 1, 1]
+        ];
+    }
+
+    /**
+     * @depends test_it_can_be_instantiated
+     * @dataProvider sizesDoesFitProvider
+     */
+    public function test_does_fit_the_box($x, $y, $z, Box $mainBox): void
+    {
+        $this->assertTrue((new Box($x, $y, $z))->fitsInto($mainBox));
+    }
+    
+    public function sizesDoesNotFitProvider(): array
+    {
+        return [
+            [10, 5, 20],
+            [11, 5, 20],
+            [10, 6, 20],
+            [10, 5, 21]
+        ];
+    }
+
+    /**
+     * @depends test_it_can_be_instantiated
+     * @dataProvider sizesDoesNotFitProvider
+     */
+    public function test_does_not_fit_the_box($x, $y, $z, Box $mainBox): void
+    {
+        $this->assertFalse((new Box($x, $y, $z))->fitsInto($mainBox));
+    }
 }
